53a90d700d3d4e2c5f7acfd5b4e7f439800a8bac
Eric Fong <ericff@gmail.com>
2017-07-06 17:00:59 +0800

    prevent re-run if not depend on ownProps

--- src/connect.js
+++ src/connect.js
@@ -7,23 +7,29 @@ import { connect } from 'react-redux'
 export default (mapState, mapDispatch, ...rest) => {
   let _mapState = mapState
   if (mapState) {
-    _mapState = (state, ownProps) => {
-      const store = ownProps.store
+    _mapState = (initState, initProps) => {
+      // assume store won't change
+      const store = initProps.store
       const context = store.context
-      context.duringMapState = true
-
-      const props = mapState(store, ownProps, state)
-
-      // always set back to normal mode, if some find / queries set as serverPreloading
-      context.serverPreloading = false
-      context.duringMapState = false
-      return props
+      const wrappedMapState = (state, props) => {
+        context.duringMapState = true
+        const newProps = mapState(store, props, state)
+        // always set back to normal mode, if some find / queries set as serverPreloading
+        context.serverPreloading = false
+        context.duringMapState = false
+        return newProps
+      }
+      return mapState.length > 1 ? wrappedMapState : () => wrappedMapState()
     }
   }
 
   let _mapDispatch = mapDispatch
   if (mapDispatch) {
-    _mapDispatch = (dispatch, ownProps) => mapDispatch(ownProps.store, ownProps, dispatch)
+    _mapDispatch = (initDispatch, initProps) => {
+      const store = initProps.store
+      const wrappedMapDispatch = (dispatch, props) => mapDispatch(store, props, dispatch)
+      return mapDispatch.length > 1 ? wrappedMapDispatch : () => wrappedMapDispatch()
+    }
   }
 
   return compose(getContext({ store: PropTypes.object.isRequired }), connect(_mapState, _mapDispatch, ...rest))
