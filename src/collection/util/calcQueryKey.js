69a8d1d378e084a760aefdec34f7c59e5a2360af
Eric <ericff@gmail.com>
2017-12-01 00:02:46 +0800

    some option cannot be serialise to reply worker

--- src/collection/util/calcQueryKey.js
+++ src/collection/util/calcQueryKey.js
@@ -5,7 +5,7 @@ import stringify from 'fast-stable-stringify'
 // fields for backend DB select columns
 // fetchName for backend fetch api name
 // fetch object for backend fetch parameters
-const serializingFields = ['sort', 'skip', 'limit', 'keyBy', 'keyByValue', 'groupBy', 'map', 'fields', 'fetch', 'fetchName', 'inOriginal', 'distinct']
+const serializingFields = ['sort', 'skip', 'limit', 'keyBy', 'fields', 'fetch', 'fetchName', 'inOriginal']
 
 export const pickOptionForSerialize = option => _.pick(option, serializingFields)
 
