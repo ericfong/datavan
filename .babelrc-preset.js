56d41aecd2c85a5c35952f624848d02694f92570
Eric <ericff@gmail.com>
2018-03-21 10:52:01 +0800

    fix: upgrade babel

---
+++ .babelrc-preset.js
@@ -0,0 +1,32 @@
+module.exports = ({ env }) => {
+  const envStr = env && env()
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          es6: {
+            targets: { browsers: '> 1%' },
+            modules: false,
+          },
+          cjs: {
+            targets: { uglify: true },
+          },
+          test: { targets: { node: 'current' } },
+        }[envStr],
+      ],
+      envStr === 'test' && '@babel/preset-react',
+    ].filter(Boolean),
+
+    plugins: [
+      '@babel/plugin-proposal-object-rest-spread',
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-export-default-from',
+
+      'lodash',
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-pipeline-operator',
+    ],
+  }
+}
